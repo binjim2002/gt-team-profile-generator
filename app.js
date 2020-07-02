a2ab20d022a06da44a422fde53ac4006de9b8b09
binjim2002 <binjim2002@yahoo.fr>
2020-07-01 20:45:12 -0400

    initial changes

--- app.js
+++ app.js
@@ -1,14 +1,100 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./Develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("./Develop/lib/htmlRenderer");
+
+
+const newEmployeeQuestions = [
+    {
+        name: 'name',
+        message: 'What is the name of the employee?'
+    },
+    {
+        name:'id',
+        message: 'What is the employee\'s id?'
+    },
+    {
+        name:'email',
+        message:'What is the employee\'s email?'
+    },
+    {
+        name:'dynamic',
+        message:''
+    }
+];
+
+const dictionary = {
+    Manager,
+    Engineer,
+    Intern
+}
+
+class CliTool{
+    mainMenu(){
+        this.ask([
+            {
+                name: 'action',
+                message: 'What do you want to do?',
+                type: 'list',
+                choices: ['add employee', 'render page']
+            }
+        ],task =>{
+            switch(task.action){
+                case 'add employee':
+                    this.addEmployee();
+                    break;
+                case 'render page':
+                    this.render();
+                    break;
+            }
+           
+        })
+    }
+    addEmployee(){
+        const questions = [{
+            name:'newEmployee',
+            message: 'What kind of employee do you want to add?',
+            type:'list',
+            choices:['Manager','Engineer','Intern']
+        }]
+        this.ask(questions,answer=>{
+            switch(answer.newEmployee){
+                case 'Manager': newEmployeeQuestions[3].message = 'Please provide a phone number'; break;
+                case 'Engineer': newEmployeeQuestions[3].message = 'Please provide a github account'; break;
+                case 'Intern': newEmployeeQuestions[3].message = 'Which school does the intern attend?'; break;
+            }
+            
+            this.ask(newEmployeeQuestions,details=>{
+                employees.push(new dictionary[answer.newEmployee](details.name, details.id, details.email, details.dynamic));
+                this.mainMenu();
+                
+            })
+        })
+    }
+    render(){
+        fs.writeFile(path.resolve(__dirname)+'/output/team.html', render(employees), 'utf8', function(err){
+            if(err){
+                console.log(err)
+            }
+            
+        })
+    }
+    ask(questions,callback){
+        inquirer.prompt(questions)
+        .then(callback)
+    }
+}
+
+const program = new CliTool();
+program.mainMenu();
 
 
 // Write code to use inquirer to gather information about the development team members,
